0344b7c55a2e1907bc6e16488d241d52b40db711
Stephen Adhikurnia <stephen.535200056@stu.untar.ac.id>
2022-02-22 10:47:39 +0700

    Tugas Pertemuan 2

---
+++ flutter_application_1/flutter_application_1/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage('images/tesimage.jpg'),
+              ),
+              Text('Hello World'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
